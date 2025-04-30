50cf25416a08bfca341f62bf618a84589895b649
NicoAlcaide <nicolas.alcaide714@comunidadunir.net>
2025-04-30 15:50:16 +0200

    limpia

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23571, hash: '1ada74dab18fd5006419ca1097da10378f1fbe3b537b7bb98a4c971291a42ea7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17145, hash: 'b2f999439606e0fc8109c873b6c2cc9301ca3edb624cc79a0a92c1c7e89f71fc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 49746, hash: '1ed43d72e9b4e11bd885f07d8b0973d6154127a8a191166ebfdbb9f146ab7635', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23571, hash: '938ee683e1d63eb372532fc23c8eb59915933b0c65ab3c22a88e5900a2cc4ac9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17145, hash: '7d7efb45ecf0c86b92f03599bdb1565a3127a476ab91a762b936d3a91182bbe4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 49746, hash: 'b3078e53ca26d1012eb4a5e71c1b13b11dd2e26701c989b21b68b48c2d31a99a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-36AW6TKX.css': {size: 6979, hash: 'vY6tjD/ce7M', text: () => import('./assets-chunks/styles-36AW6TKX_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { EnviarDatosComponent } from './enviardatos/enviardatos.component';
+import { EnviarCsvComponent } from './enviarcsv/enviarcsv.component';
 import { ConsultarDatosComponent } from './consultardatos/consultardatos.component';
 
 const routes: Routes = [
-  { path: '', component: InicioComponent }, // ✅ Página principal
-  { path: 'consultar-datos', component: ConsultarDatosComponent }, // ✅ Ruta de consulta
-  { path: '**', redirectTo: '', pathMatch: 'full' } // ✅ Redirigir rutas desconocidas
+  { path: '', component: InicioComponent },
+  { path: 'enviodatos', component: EnviarDatosComponent },
+  { path: 'enviocsv', component: EnviarCsvComponent },
+  { path: 'consultarinfo', component: ConsultarDatosComponent }
 ];
 
 @NgModule({
