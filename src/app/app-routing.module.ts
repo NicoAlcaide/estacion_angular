95f26f60b8b9d7d22834ea5bc87e85d588c4411a
NicoAlcaide <nicolas.alcaide714@comunidadunir.net>
2025-05-16 11:19:15 +0200

    Def

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { InicioComponent } from './inicio/inicio.component';
 import { EnviarDatosComponent } from './enviardatos/enviardatos.component';
 import { EnviarCsvComponent } from './enviarcsv/enviarcsv.component';
 import { ConsultarDatosComponent } from './consultardatos/consultardatos.component';
+import { LimpiarCanalComponent } from './limpiar-canal/limpiar-canal.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'enviodatos', component: EnviarDatosComponent },
   { path: 'enviocsv', component: EnviarCsvComponent },
-  { path: 'consultarinfo', component: ConsultarDatosComponent }
+  { path: 'consultarinfo', component: ConsultarDatosComponent },
+  { path: 'limpiar', component: LimpiarCanalComponent } 
 ];
 
 @NgModule({
